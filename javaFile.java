9cae7b510e5e033318b71cc6fbf07ecdae773f5a
Ubuntu <ubuntu@ip-172-31-19-153.eu-north-1.compute.internal>
2024-11-22 05:22:44 +0000

    java file added

---
+++ javaFile.java
@@ -0,0 +1,8 @@
+public class javaFile {
+    public static void main(String[] args) {
+        // Replace with your registration number
+        String registrationNumber = "FA21-BCS-032";
+
+        System.out.println("My Registration Number is: " + registrationNumber);
+    }
+}
